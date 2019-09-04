8fffac9443d59ef5f03512b7b94f16c3d0297ebb
Gopal <gopal125dan@gmail.com>
2019-09-05 00:22:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gopal/downloadfileusingurl/DownloadTask.java
@@ -0,0 +1,262 @@
+package com.example.gopal.downloadfileusingurl;
+
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by SONU on 29/10/15.
+ */
+public class DownloadTask {
+
+    private static final String TAG = "Download Task";
+    private Context context;
+    private Button buttonText;
+    private String downloadUrl = "", downloadFileName = "";
+
+    public DownloadTask(Context context, Button buttonText, String downloadUrl) {
+        this.context = context;
+        this.buttonText = buttonText;
+        this.downloadUrl = downloadUrl;
+
+        downloadFileName = downloadUrl.replace(Utils.mainUrl, "");//Create file name by picking download file name from URL
+        Log.e(TAG, downloadFileName);
+
+        //Start Downloading Task
+        new DownloadingTask().execute();
+    }
+
+    private class DownloadingTask extends AsyncTask<Void, Void, Void> {
+
+        File apkStorage = null;
+        File outputFile = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            buttonText.setEnabled(false);
+            buttonText.setText(R.string.downloadStarted);//Set Button Text when download started
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            try {
+                if (outputFile != null) {
+                    buttonText.setEnabled(true);
+                    buttonText.setText(R.string.downloadCompleted);//If Download completed then change button text
+                } else {
+                    buttonText.setText(R.string.downloadFailed);//If download failed change button text
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonText.setEnabled(true);
+                            buttonText.setText(R.string.downloadAgain);//Change button text again after 3sec
+                        }
+                    }, 3000);
+
+                    Log.e(TAG, "Download Failed");
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                //Change button text if exception occurs
+                buttonText.setText(R.string.downloadFailed);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        buttonText.setEnabled(true);
+                        buttonText.setText(R.string.downloadAgain);
+                    }
+                }, 3000);
+                Log.e(TAG, "Download Failed with Exception - " + e.getLocalizedMessage());
+
+            }
+
+
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                URL url = new URL(downloadUrl);//Create Download URl
+                HttpURLConnection c = (HttpURLConnection) url.openConnection();//Open Url Connection
+                c.setRequestMethod("GET");//Set Request Method to "GET" since we are grtting data
+                c.connect();//connect the URL Connection
+
+                //If Connection response is not OK then show Logs
+                if (c.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    Log.e(TAG, "Server returned HTTP " + c.getResponseCode()
+                            + " " + c.getResponseMessage());
+
+                }
+
+
+                //Get File if SD card is present
+                if (new CheckForSDCard().isSDCardPresent()) {
+
+                    apkStorage = new File(
+                            Environment.getExternalStorageDirectory() + "/"
+                                    + Utils.downloadDirectory);
+                } else
+                    Toast.makeText(context, "Oops!! There is no SD Card.", Toast.LENGTH_SHORT).show();
+
+                //If File is not present create directory
+                if (!apkStorage.exists()) {
+                    apkStorage.mkdir();
+                    Log.e(TAG, "Directory Created.");
+                }
+
+                outputFile = new File(apkStorage, downloadFileName);//Create Output file in Main File
+
+                //Create New File if not present
+                if (!outputFile.exists()) {
+                    outputFile.createNewFile();
+                    Log.e(TAG, "File Created");
+                }
+
+                FileOutputStream fos = new FileOutputStream(outputFile);//Get OutputStream for NewFile Location
+
+                InputStream is = c.getInputStream();//Get InputStream for connection
+
+                byte[] buffer = new byte[1024];//Set buffer type
+                int len1 = 0;//init length
+                while ((len1 = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len1);//Write new file
+                }
+
+                //Close all connection after doing task
+                fos.close();
+                is.close();
+
+            } catch (Exception e) {
+
+                //Read exception if something went wrong
+                e.printStackTrace();
+                outputFile = null;
+                Log.e(TAG, "Download Error Exception " + e.getMessage());
+            }
+
+            return null;
+        }
+    }
+
+    private void makeHttpConnection(String stringUrl){
+        try {
+            URL url = new URL(downloadUrl);//Create Download URl
+            HttpURLConnection c = (HttpURLConnection) url.openConnection();//Open Url Connection
+            c.setRequestMethod("GET");//Set Request Method to "GET" since we are grtting data
+            c.connect();//connect the URL Connection
+            //If Connection response is not OK then show Logs
+            if (c.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                File externalFile = null;
+                Log.e(TAG, "Server returned HTTP " + c.getResponseCode() + " " + c.getResponseMessage());
+                InputStream is = c.getInputStream();//Get InputStream for connection
+                FileOutputStream fos = new FileOutputStream(externalFile);//Get OutputStream for NewFile Location
+                byte[] buffer = new byte[1024];//Set buffer type
+                int len1 = 0;//init length
+                while ((len1 = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len1);//Write new file
+                }
+
+                //Close all connection after doing task
+                fos.close();
+                is.close();
+
+             /*   // input stream to read file - with 8k buffer
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                // Output stream to write file
+                OutputStream output = new FileOutputStream("/sdcard/downloadedfile.jpg");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress(""+(int)((total*100)/lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();*/
+
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            // 1 Forming the HTTP request
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            //2  Establishing the HTTP connection/request
+            urlConnection.connect();
+
+            // 3 Receiving the response
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                //jsonResponse = readFromStream(inputStream);
+            } else {
+               // Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            // Use getErrorStream() to read the error response.
+           // Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            // Disconnecting releases the resources held by a connection so they may be closed or reused.
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+
+}
+
